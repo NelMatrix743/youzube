dd9ad7e2f417f69319f28b535f994c98c90c6d78
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-02-17 17:53:54 +0000

    feat(data): create user profile model for auth events

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class UserProfile(models.Model):
+
+    user: models.OneToOneField[User] = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="profile"  # lets you access it as user.profile.phone_number
+    )
+    phone_number: models.CharField = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f"""
+USERNAME: {self.user.username}
+EMAIL: {self.user.email}
+PHONE NUMBER: {self.phone_number}
+"""

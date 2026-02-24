19d27ced623ea0b5369305c8c6aff238743d09d5
Nelson Chidi (Nelmatrix743) <nelmatrix155@gmail.com>
2026-02-24 09:47:58 +0000

    feat: implement a function for generating thumbnail from first video frame

--- apps/videos/imagekit_client.py
+++ apps/videos/imagekit_client.py
@@ -51,3 +51,5 @@ def get_streaming_url(base_url: str) -> str:
     return f"{base_url}/ik-master.m3u8?tr=sr-360_480_720_1080"
 
 
+def get_thumbnail_url(base_url: str, width: int = 480, height: int = 270) -> str:
+    return f"{base_url}/ik-thumbnail.jpg"
